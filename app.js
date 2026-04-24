b97b3e7ea876655d43e539de3d949fe361f2b462
digantaduworah <digantaduworah@gmail.com>
2026-04-24 16:39:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -45,21 +45,34 @@ function showQR() {
 }
 
 function submitOrder() {
-  let phone = document.getElementById("phone").value;
-  let service = document.getElementById("service").value;
-  let amount = document.getElementById("total").innerText;
+  try {
+    let phone = document.getElementById("phone").value;
+    let service = document.getElementById("service").value;
+    let amount = document.getElementById("total").innerText;
 
-  let orderId = "CSC" + Date.now();
+    if (!phone || !service) {
+      alert("Fill all fields");
+      return;
+    }
 
-  db.collection("orders").add({
-    phone,
-    service,
-    amount,
-    orderId,
-    status: "Pending"
-  });
+    let orderId = "CSC" + Date.now();
 
-  alert("Order Placed!");
+    db.collection("orders").add({
+      phone,
+      service,
+      amount,
+      orderId,
+      status: "Pending",
+      time: new Date().toString()
+    }).then(() => {
+      alert("Order Submitted Successfully!");
+      document.getElementById("qrBox").style.display = "none";
+    });
+
+  } catch (e) {
+    console.log(e);
+    alert("System Error");
+  }
 }
 
 function trackOrder() {
